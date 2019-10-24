81fcfbce7c762bd2061432510ff1368bab595d2e
Yanis DANDO <yanis.dando@gmail.com>
2019-10-24 14:17:10 +0200

    cash selector

--- Assets/Instantiate.cs
+++ Assets/Instantiate.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Vuforia;
 
 public class Instantiate : DefaultTrackableEventHandler
 {
     public int somme;
 
-    private int[] billets = {500,200,100,50,20,10,5,1};
+    private List<int> billets = new List<int>();  //{500,200,100,50,20,10,5,1};
     public Transform[] prefabs = {};
+
+    public Toggle b500; 
+    public Toggle b200;
+    public Toggle b100;
+    public Toggle b50;
+    public Toggle b20;
+    public Toggle b10;
+    public Toggle b5;
+    public Toggle b1;
+    private List<Toggle> selections;
 
     protected override  void OnTrackingFound()
     {
+        Setup();
+
         int cpt = 0;
         int nbBillets = 0;
         int calc = somme;
@@ -27,66 +40,34 @@ public class Instantiate : DefaultTrackableEventHandler
                 calc -= billets[cpt];
 
                 Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/somme));
-                Instantiate(prefabs[cpt], objectPosition, Quaternion.identity);
+                Transform prefabToCreate = prefabs[0];
+                for (int k = 0; k < prefabs.Length; k++)
+                {
+                    if(int.Parse(prefabs[k].name) == billets[cpt]){
+                        prefabToCreate = prefabs[k];
+                    }
+                }
+                Instantiate(prefabToCreate, objectPosition, Quaternion.identity);
             }
             cpt++;
         } while (calc != 0);
 
-        /*switch (6)
-        {
-            case 0:
-                for(int i = 0; i<nombre; i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(piece1, objectPosition, Quaternion.identity);
-                }
-                break;
-            case 1:
-                for(int i = 0; i<(nombre/5); i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(billet5, objectPosition, Quaternion.identity);
-                }
-                break;
-            case 2:
-                for(int i = 0; i<(nombre/10); i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(billet10, objectPosition, Quaternion.identity);
-                }
-                break;
-            case 3: 
-                for(int i = 0; i<(nombre/20); i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(billet20, objectPosition, Quaternion.identity);
-                }
-                break;
-            case 4: 
-                for(int i = 0; i<(nombre/50); i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(billet50, objectPosition, Quaternion.identity);
-                }
-                break;
-            case 5:
-                for(int i = 0; i<(nombre/100); i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(billet100, objectPosition, Quaternion.identity);
-                }
-                break;
-            case 6:
-                for(int i = 0; i<(nombre/200); i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(billet200, objectPosition, Quaternion.identity);
-                }
-                break;
-            case 7:
-                for(int i = 0; i<(nombre/500); i++) {
-                    Vector3 objectPosition = Vector3.Lerp(new Vector3(0,0,150), new Vector3(0,i/2,150), (float)i * (1f/nombre));
-                    Instantiate(billet500, objectPosition, Quaternion.identity);
-                }
-                break;
-        }*/
     }
 
     protected override void OnTrackingLost()
     {
+        billets.Clear();
+        selections.Clear();
         base.OnTrackingLost ();
+    }
+
+    private void Setup() {
+        selections = new List<Toggle> {b500, b200, b100, b50, b20, b10, b5, b1};
+
+        foreach (Toggle t in selections) {
+            if (t.isOn == true) { 
+                billets.Add(int.Parse(t.GetComponentInChildren<Text> ().text));
+            }
+        }
     }
 }
